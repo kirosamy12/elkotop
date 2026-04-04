c804e6de16bec54b08536d0644b5aaa1a1526545
YAS <yas@MacBook-Air.local>
2026-04-04 15:07:33 +0200

    fix: explicit mysql2 import for Vercel

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    dialectModule: mysql2,
     logging: false,
     dialectOptions: {
       connectTimeout: 60000
