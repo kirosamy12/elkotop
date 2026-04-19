268d089f12d1ccc08b1b771a47a2d362f8b8fec7
YAS <yas@MacBook-Air.local>
2026-04-19 22:15:50 +0200

    fix: remove next from mongoose pre save hooks

--- modules/author/author.model.js
+++ modules/author/author.model.js
@@ -4,6 +4,16 @@ const authorSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   bio: { type: String, trim: true },
   image: { type: String, default: '' }
-}, { timestamps: true });
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    }
+  }
+});
 
 export default mongoose.model('Author', authorSchema);
--- modules/category/category.model.js
+++ modules/category/category.model.js
@@ -2,6 +2,16 @@ import mongoose from 'mongoose';
 
 const categorySchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true, trim: true }
-}, { timestamps: true });
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    }
+  }
+});
 
 export default mongoose.model('Category', categorySchema);
